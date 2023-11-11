974fdf3d07314218480f2fae6639b6636ed51d81
Jami <jamsheduddin03@gmail.com>
2023-11-11 14:50:57 +0600

    server initiated

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("morsel server is on");
+});
+
+app.listen(port, () => {
+  console.log("morsel server is running");
+});
